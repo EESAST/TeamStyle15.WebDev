ed214c0d2fa2326e13a82dc302c6c21c721f660f
Blaok <ssrs1994@126.com>
2013-07-12 22:42:47 +0800

    BrandNew
    全新的Rails框架
    准备了3.2.6和4.0.0两个版本
    计划先使用Rails3进行开发，如果可能再升级Rails4

---
+++ Rails3.2.6/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails326::Application.config.secret_token = '668ca0cd9b83865624dfdc7ed5cec011479e75c9b86cddaf564d8007b2dc6536f2f373c3f9bcceb1b8002d79276e97364311f05b79ee7212aad721cb8e8b5a44'
---
+++ Rails4.0.0/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails400::Application.config.secret_key_base = 'c90b44f877d8b6e5bd00262d1d0b1bc530725f7630c3a951e21a1c73dd0858434fde413366085e0ee4199e78b1bbca2b3f474f84fc8a3bd03cc82570987bb868'
