1351e68e2025fa0a41974b3c3154ac75ce48cb5e
Blaok <ssrs1994@126.com>
2013-07-17 20:40:21 +0800

    AddedHomeIndex
    添加了主页。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamStyle15Webdev::Application.config.secret_key_base = 'b1c9ab1482101a56bd181e60ae1b169189e94f6d2ed0a892539210653b3d951b6443f68b0ff185537c77ebd87313003c19e24255a40a4f8add5241535d26bf77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamStyle15Webdev::Application.config.session_store :cookie_store, key: '_TeamStyle15_WebDev_session'
