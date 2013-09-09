5415bb5d018ffceba91edc3a34ee0991d323925e
Blaok <blaok@Blaok-Lenovo-G470.(none)>
2013-09-09 17:13:46 +0800

    Merge以后又做了一些小的改动

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,5 +21,23 @@
       redirect_to root_path, :notice => "您不是管理员"
     end
   end
+
+  def self.rescue_errors
+     rescue_from Exception,                            :with => :render_error
+     rescue_from RuntimeError,                         :with => :render_error
+     rescue_from ActiveRecord::RecordNotFound,         :with => :render_not_found
+     rescue_from ActionController::RoutingError,       :with => :render_not_found
+     rescue_from ActionController::UnknownController,  :with => :render_not_found
+     #rescue_from ActionController::UnknownAction,      :with => :render_not_found
+   end
+   rescue_errors unless Rails.env.development?
+   
+   def render_not_found(exception = nil)
+     render :template => "errors/404", :status => 404, :layout => 'public'
+   end
+   
+   def render_error(exception = nil)
+     render :template => "errors/500", :status => 500, :layout => 'public'
+   end
   
 end
