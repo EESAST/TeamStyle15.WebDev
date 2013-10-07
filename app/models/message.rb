c71f11d2eb1c2adb5d34b9620332c3b5f5e76b7d
Blaok <ssrs1994@126.com>
2013-10-07 14:58:29 +0800

    First version for release

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,11 +8,14 @@ class Message < ActiveRecord::Base
     #3：用户入队申请
     #4：加入队伍邀请
     #5：@
+    #6：私信
   #content：根据消息类别不同的消息内容的id
     #1：评论id
     #3：申请入队用户id
     #4：发邀队伍id
+    #6：发信人id
   #read：是否已读
   #text：附加信息
+    #6：信息内容
 
 end
